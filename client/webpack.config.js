054cd8bdd9600816ecc56986ea24078768151282
Lauren <lburke522@gmail.com>
2022-10-14 14:23:51 -0400

    module rules filled in

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve('client/src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
@@ -46,7 +46,24 @@ module.exports = () => {
 
     module: {
       rules: [
-       
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
